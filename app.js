002694053f12b941b29068dc21ace46a288e0958
Martin Laws <martinblaws@gmail.com>
2020-02-26 03:15:49 -0500

    latest code before 02/27 lecture

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 // document.addEventListener('DOMContentLoaded', () => {
 // })
 
-const getUserInput = () => $('#subreddit-input').val()
+const getUserInput = () => $("#subreddit-input").val();
 
 const fetchAndRenderRedditPosts = () => {
   $.ajax({
@@ -11,11 +11,10 @@ const fetchAndRenderRedditPosts = () => {
     dataType: "JSON"
   })
     .then(response => {
-      const renderedPosts = formatRedditPosts(response.data.children)
+      const renderedPosts = formatRedditPosts(response.data.children);
 
-      $('#app').append(renderedPosts)
+      $("#app").append(renderedPosts);
       // document.getElementById('app').append(renderedPosts)
-
     })
     .catch(() => {
       const errorMessage = `
@@ -23,9 +22,9 @@ const fetchAndRenderRedditPosts = () => {
           <h1>Whoops, something went wrong!</h1>
           <p>Please try a different subreddit or call support at 967-1111</p>
         </div>
-      `
+      `;
 
-      $('#app').append(errorMessage)
+      $("#app").append(errorMessage);
     });
 };
 
@@ -33,31 +32,27 @@ const formatRedditPosts = posts => {
   const markupArray = [];
 
   for (post of posts) {
-    const {
-      title, 
-      permalink, 
-      thumbnail
-    } = post.data;
+    const { title, permalink, thumbnail } = post.data;
 
     if (isThumbnailValid(thumbnail)) {
       markupArray.push(`
         <a href="https://www.reddit.com${permalink}" title="${title}" target="_blank">
           <img alt="${title}" src="${thumbnail}" />
         </a>
-      `)
+      `);
     }
   }
 
-  return markupArray.join('');
-}
+  return markupArray.join("");
+};
 
 // This function checks if the thumbnail value matches one of our known invalid cases
 const isThumbnailValid = thumbnailURL => {
-  const blacklistedValues = ['self', undefined, null, 'unknown', '', 'default']
+  const blacklistedValues = ["self", undefined, null, "unknown", "", "default"];
 
   if (blacklistedValues.includes(thumbnailURL)) {
-    return false
+    return false;
   }
 
-  return true
-}
+  return true;
+};
--- completeAppWithImageNotThumbnail.js
+++ completeAppWithImageNotThumbnail.js
@@ -5,58 +5,58 @@
 //     dataType: 'JSON'
 //   }).then( response => {
 //     const renderedPosts = renderPosts(response.data.children)
-    
+
 //     $('#app').append(renderedPosts)
 //   }).catch(error => {
 //     console.error(error)
 //   })
 // }
 
-const fetchAndRenderRedditPosts = async () =>  {
+const fetchAndRenderRedditPosts = async () => {
   try {
     const response = await $.ajax({
       url: `https://www.reddit.com/r/${getUserInput()}.json`,
-      type: 'GET',
-      dataType: 'JSON'
-    })
+      type: "GET",
+      dataType: "JSON"
+    });
 
-    const renderedPosts = renderPosts(response.data.children)
-    
-    $('#app').append(renderedPosts)
+    const renderedPosts = renderPosts(response.data.children);
+
+    $("#app").append(renderedPosts);
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
-}
+};
 
 const isImageValid = imageUrl => {
-  if (imageUrl === 'self' && !imageUrl) {
-    return false
+  if (imageUrl === "self" && !imageUrl) {
+    return false;
   } else {
-    return true
+    return true;
   }
-}
+};
 
 const getUserInput = () => {
-  const subredditInput = $('#subreddit-input')
+  const subredditInput = $("#subreddit-input");
 
-  return subredditInput.val()
-}
+  return subredditInput.val();
+};
 
 const renderPosts = posts => {
-  const renderedPostsArray = []
+  const renderedPostsArray = [];
 
   for (const post of posts) {
-    const { title, permalink } = post.data
-    const imageUrl = post.data.preview.images[0].source.url
+    const { title, permalink } = post.data;
+    const imageUrl = post.data.preview.images[0].source.url;
 
     if (isImageValid(imageUrl)) {
       renderedPostsArray.push(`
         <a href="https://www.reddit.com${permalink}" title="${title}" target="_blank">
           <img alt="${title}" src="${imageUrl}" />
         </a>
-      `)
+      `);
     }
   }
-  
-  return renderedPostsArray.join('')
-}
+
+  return renderedPostsArray.join("");
+};
